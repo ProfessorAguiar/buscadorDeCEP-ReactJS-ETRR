43098e770a1151d5091ff5a84e40c1b76de9797e
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2024-05-16 09:30:48 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Buscador de CEP</h1>
+      <input type="text" name="CEP" id="CEP" />
+      <button>Buscar</button>
+    </>
+  )
+}
