963e140eda8a880fe4a1a1b233242edb93eafd29
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2024-05-23 12:46:15 -0300

    23-05

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import './App.css'
-
+import axios from 'axios'
+import { useState } from 'react'
 export default function App() {
-
+  const [CEP, setCEP] = useState('')
+  const [endereco, setEndereco]=useState('')
+  const [bairro, setBairro]=useState('')
+  const [cidade, setCidade]=useState('')
+  const [estado, setEstado]=useState('')
+  function buscarCEP() {
+    const options = {
+      method: 'GET',
+      url: `http://viacep.com.br/ws/${CEP}/json/`
+    };
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      setEndereco(`Endereço: ${response.data.logradouro}`)
+      setBairro(`Bairro: ${response.data.bairro}`)
+      setCidade(`Cidade: ${response.data.localidade}`)
+      setEstado(`Estado: ${response.data.uf}`)
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
   return (
     <>
-      <h1>Buscador de CEP</h1>
-      <input type="text" name="CEP" id="CEP" />
-      <button>Buscar</button>
+      <div className='app'>
+        <h1>Buscador de CEP</h1>
+        <input type="text" onChange={e => { setCEP(e.target.value) }} />
+        <button onClick={buscarCEP} className='buscar'>Buscar</button>
+        <p>{endereco}</p>
+        <p>{bairro}</p>
+        <p>{cidade}</p>
+        <p>{estado}</p>
+      </div>
     </>
   )
 }
